70f36eb62f56d368cea48d8575389c317327bdf6
thsvkd <thsxogud1123@gmail.com>
2023-10-07 04:38:35 +0900

    Remove unused code
    Signed-off-by: thsvkd <thsxogud1123@gmail.com>

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from discord import Intents
 from discord.message import Message
-from discord.ext import commands
-from ksbot import KSBot
 import os
 
 from utils import CustomHelpCommand
+from ksbot import KSBot
 
 
 ks_bot = KSBot(command_prefix='!', intents=Intents.all())
@@ -19,15 +18,6 @@ async def hello(message: Message):
 @ks_bot.command(description="예의 주입할 것")
 async def fuckyou(message: Message):
     await message.channel.send('응, 꺼져~')
-
-
-# @ks_bot.command(name="help", description="Returns all commands available")
-# async def help(message: Message):
-#     helptext = "```"
-#     for command in ks_bot.commands:
-#         helptext += f"{command}\n"
-#     helptext += "```"
-#     await message.send(helptext)
 
 
 DISCORD_TOKEN = os.environ.get('DISCORD_TOKEN', '')
